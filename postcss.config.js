8126a12e91fbc17231ef6b4e18884688cf41569d
Greg Mayes <gregmayes@gmail.com>
2023-11-12 16:44:38 +0000

    Site foundations

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    plugins: [
+        require('autoprefixer'),
+        require('tailwindcss'),
+        ...(process.env.JEKYLL_ENV === 'production' ? [require('cssnano')({ preset: 'default' })]: []),
+    ]
+};
